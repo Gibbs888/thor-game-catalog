e4bddd54f3ff0baef953a36df212a1d6bd451730
Gibbs888 <Gibbs1912@windowslive.com>
2026-07-19 13:32:13 +0200

    Update Game.kt for platform website settings

--- app/src/main/java/com/gibbstech/thorgamecatalog/Game.kt
+++ app/src/main/java/com/gibbstech/thorgamecatalog/Game.kt
@@ -7,63 +7,54 @@ enum class Platform(
     val label: String,
     val shortLabel: String,
     val thumbnailRepository: String,
-    val officialDomain: String,
 ) {
     PS1(
         id = "ps1",
         label = "PlayStation 1",
         shortLabel = "PS1",
         thumbnailRepository = "Sony_-_PlayStation",
-        officialDomain = "playstation.com",
     ),
     PS2(
         id = "ps2",
         label = "PlayStation 2",
         shortLabel = "PS2",
         thumbnailRepository = "Sony_-_PlayStation_2",
-        officialDomain = "playstation.com",
     ),
     PSP(
         id = "psp",
         label = "PlayStation Portable",
         shortLabel = "PSP",
         thumbnailRepository = "Sony_-_PlayStation_Portable",
-        officialDomain = "playstation.com",
     ),
     GAMECUBE(
         id = "gamecube",
         label = "Nintendo GameCube",
         shortLabel = "GC",
         thumbnailRepository = "Nintendo_-_GameCube",
-        officialDomain = "nintendo.com",
     ),
     WII(
         id = "wii",
         label = "Nintendo Wii",
         shortLabel = "Wii",
         thumbnailRepository = "Nintendo_-_Wii",
-        officialDomain = "nintendo.com",
     ),
     DREAMCAST(
         id = "dreamcast",
         label = "SEGA Dreamcast",
         shortLabel = "DC",
         thumbnailRepository = "Sega_-_Dreamcast",
-        officialDomain = "sega.com",
     ),
     NDS(
         id = "nds",
         label = "Nintendo DS",
         shortLabel = "DS",
         thumbnailRepository = "Nintendo_-_Nintendo_DS",
-        officialDomain = "nintendo.com",
     ),
     N3DS(
         id = "n3ds",
         label = "Nintendo 3DS",
         shortLabel = "3DS",
         thumbnailRepository = "Nintendo_-_Nintendo_3DS",
-        officialDomain = "nintendo.com",
     );
 
     companion object {
@@ -78,6 +69,7 @@ data class Game(
     val year: Int,
     val description: String,
     val thumbnailName: String,
+    val region: String = "EU",
 ) {
     val coverUrl: String
         get() {
